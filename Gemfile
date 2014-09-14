9d1be4d25cbf2d2c53ef25033b5fc7e939c40c64
shotsoft <shotsoftsolutions@gmail.com>
2014-09-14 18:48:51 -0400

    heroku predeploy

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'jquery-rails', '3.1.1'
 gem 'turbolinks',   '2.2.2'
 gem 'jbuilder',     '2.1.3'
 gem 'sdoc',         '0.4.0', group: :doc
+ruby "2.1.2"
 
 group :test do
   gem 'minitest-reporters', '1.0.5'
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -21,5 +21,11 @@ class StaticPagesControllerTest < ActionController::TestCase
   	assert_select "title", "Ruby on Rails Tutorial Sample App | About"
 
   end
+   
+  test "should get contact" do
+  	get :contact
+  	assert_response :success
+  	assert_select "title", "Ruby on Rails Tutorial Sample App | About"
+
 
 end
