850b6e846b4cf128ea125e37d4e2a2b5e58cb21f
shotsoft <shotsoftsolutions@gmail.com>
2014-09-14 18:53:05 -0400

    route fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
-  get 'static_pages/help' => 'static_pages#index'
-  get 'static_pages/about' => 'static_pages#index'
+  get 'static_pages/help' => 'static_pages#help'
+  get 'static_pages/about' => 'static_pages#help'
+  get 'static_pages/contact' => 'static_pages#contact'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
