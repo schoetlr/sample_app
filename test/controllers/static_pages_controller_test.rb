f6519e3737736390f0f5bbf5b6623935fc38dfd6
shotsoft <shotsoftsolutions@gmail.com>
2014-09-14 20:58:59 -0400

    small changes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,28 +4,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App | Home"
-
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
     get :help
     assert_response :success
     assert_select "title", "Ruby on Rails Tutorial Sample App | Help"
-
   end
 
   test "should get about" do
-  	get :about
-  	assert_response :success
-  	assert_select "title", "Ruby on Rails Tutorial Sample App | About"
-
+    get :about
+    assert_response :success
+    assert_select "title", "Ruby on Rails Tutorial Sample App | About"
   end
-   
-  test "should get contact" do
-  	get :contact
-  	assert_response :success
-  	assert_select "title", "Ruby on Rails Tutorial Sample App | About"
-
-
 end
